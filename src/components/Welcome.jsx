0f303467e164a5a3cffc92a1d9e32e1945fb0f30
ajaydev <ajaydevzzz30@gmail.com>
2025-04-26 16:11:30 +0530

    Modified Welcome.jsx and Welcome.test.jsx for initial testing

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Welcome({name}) {
   return (
     <div>
-      <p>Hello, hi, { name || 'Guest' }</p>
+      <p>Hello, { name || 'Guest' }</p>
     </div>
   )
 }
